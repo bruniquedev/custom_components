f7802348b31051d0b923379f62d6a83b14ed6178
Bruno <bruniquedevelopers@gmail.com>
2022-10-03 23:07:42 +0300

    side navigation bar gained shape

--- Navigationbars/side-nav.php
+++ Navigationbars/side-nav.php
@@ -61,6 +61,7 @@ color: #fff;
     margin-top: 9px;
     margin-left: 170px;
     font-size: 19px;
+    cursor: pointer;
 }
 
 
@@ -261,9 +262,43 @@ ul.menu-nav li, .custom-dropdown .dropbtn {
     bottom: 0;
     overflow: visible;
     z-index: 10;
+   /* transition: width .5s ease;*/
+    transition: width 0.5s ease-out;
+    
 }
 .sidebar-container{
       height: 100%;
+}
+
+/*navigation scrollbar*/
+.sidebar-scroll{
+    height: 100vh;
+  overflow-y:scroll;/*show scroll bar*/ 
+  overflow-x:hidden;/*hide scroll bar*/
+  scroll-behavior: smooth;
+ -webkit-transition: all .5s;
+}
+
+
+.sidebar-scroll::-webkit-scrollbar {
+    background-color: #999;
+    width: 0px;/*initially 2px*/
+      border-radius: 10px;
+}
+.sidebar-scroll:hover::-webkit-scrollbar {
+    width: 10px;/*initially 10px on hover*/
+     -webkit-transition: all .5s;
+}
+
+.sidebar-scroll::-webkit-scrollbar-track {
+  border-radius: 10px;
+}
+
+.sidebar-scroll::-webkit-scrollbar-thumb {
+  margin: 2px;
+ border: 10px solid transparent;
+   border-radius: 10px;
+
 }
 
 .navbar-title {
@@ -351,14 +386,32 @@ moz-transition: background-color 300ms linear;
     color: #c8c7cc;
     font-size: 18px;
 }
+/*switching icons*/
+.sidebar-nav > ul li a .icon-arrow:before {
+    font-family: Ionicons;
+  content: "\f3cf";
+
+}
+.sidebar-nav > ul li.open > a .icon-arrow:before {
+   font-family: Ionicons;
+  content: "\f3d0";
+}
 
 ul.sub-menu {
     background: #111b23;
     clear: both;
     list-style-type: none;
     padding: 1px 0;
-    display: none;
     margin: 0;
+  /* hides sub-menu */
+   /*display: none;*/
+ height: 0px;
+  visibility: hidden; 
+  overflow: hidden;
+  opacity: 0;
+  transform: translateY(-2em);
+  transition: all 0.3s;
+
 }
 ul.sub-menu li {
     background: none;
@@ -367,6 +420,7 @@ ul.sub-menu li {
     margin-right: 0;
     margin-top: 1px !important;
     padding: 0;
+
 }
 ul.sub-menu > li a {
     color: #b4bcc8;
@@ -386,12 +440,83 @@ ul.sub-menu li a {
     text-decoration: none;
 }
 
-ul.sub-menu.open {
-display: block;
+ul.sub-menu.open {/*show sub menu*/
+/*display: block;*/
+/* shows sub-menu */
+height: 100%;
+ visibility: visible; 
+  opacity: 1;
+  transform: translateY(0%);
+  transition-delay: 0s, 0s, 0.3s; /* this removes the transition delay so the menu will be visible while the other styles transition */
 }
 ul.sub-menu li:hover {
   background: #0e161c;
 }
+
+
+
+/*manipulate sidebar closed*/
+.sidebar.sidebar-closed{
+    width: 70px !important;
+     transition: width 0.3s ease-out;
+  }
+.sidebar-closed-brandtext-left{
+  display: none !important;
+}
+.sidebar-closed-brand-left-icon{
+margin-left:0px !important;
+}
+.nav-brand-left-sidebar-closed {
+    margin-left: 23px !important;
+}
+.navbar-title-sidebar-closed{
+  display: none !important;
+}
+.item-inner-sidebar-closed{
+  display: none !important;
+    visibility: hidden !important;
+    opacity: 0 !important;
+} 
+.item-content-sidebar-closed {
+    display: flex !important;
+    justify-content: center !important;
+}
+
+.main-content-sidebar-closed {
+margin-left: 70px !important;
+}
+
+
+
+.sub-menu-sidebar-closed{
+position: absolute !important;
+/*width: 100%;*/
+}
+
+/*.custom-dropdown-sidebar-closed:hover  ul.sub-menu-sidebar-closed {*/
+  /*show dropdown on hover*/
+  .custom-dropdown-sidebar-closed:hover .sub-menu-sidebar-closed{ 
+ /* display: block !important;*/
+ /* shows sub-menu */
+height: auto !important;
+ visibility: visible !important; 
+  opacity: 1 !important;
+  /*transform: translateY(0%) !important;*/
+  transition-delay: 0s, 0s, 0.3s !important; /* this removes the transition delay so the menu will be visible while the other styles transition */
+
+}
+
+/*main content area*/
+.main-content{
+  margin-top: 0;
+  margin-left: 260px;
+  min-height: 100%;
+}
+.wrap-content{
+    margin-left: 10px;
+}
+
+
 
 
 /*///////////////////////---------others ------------*/
@@ -431,7 +556,7 @@ footer{
 
 
 <div class="sidebar" id="sidebar">
-  <div class="sidebar-container">
+  <div class="sidebar-container sidebar-scroll">
 <div class="sidebar-nav">
 
 <div class="navbar-title">
@@ -466,7 +591,7 @@ footer{
 </li>
 
 
-  <li class="custom-dropdown">
+  <li class="custom-dropdown-menu">
   <a href="javascript:void(0)">
     <div class="item-content">
       <div class="item-media">
@@ -474,7 +599,7 @@ footer{
       </div>
       <div class="item-inner">
         <span class="title"> Dropdown </span>
-        <i class="ion ion-ios-arrow-back icon-arrow"></i>
+        <i class="ion icon-arrow"></i>
       </div>
     </div>
   </a>
@@ -492,7 +617,7 @@ footer{
   </ul>
   </li>
 
-  <li class="custom-dropdown">
+  <li class="custom-dropdown-menu">
   <a href="javascript:void(0)">
     <div class="item-content">
       <div class="item-media">
@@ -500,7 +625,7 @@ footer{
       </div>
       <div class="item-inner">
         <span class="title"> Dropdown 2</span>
-        <i class="ion ion-ios-arrow-back icon-arrow"></i>
+        <i class="ion  icon-arrow"></i>
       </div>
     </div>
   </a>
@@ -517,6 +642,136 @@ footer{
     </li>
   </ul>
   </li>
+
+ <li class="custom-dropdown-menu">
+  <a href="javascript:void(0)">
+    <div class="item-content">
+      <div class="item-media">
+        <i class="ion ion-help"></i>
+      </div>
+      <div class="item-inner">
+        <span class="title"> Dropdown 3</span>
+        <i class="ion icon-arrow"></i>
+      </div>
+    </div>
+  </a>
+  <ul class="sub-menu">
+    <li>
+      <a href="#">
+        <span class="title">Link 1</span>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <span class="title">Link 2</span>
+      </a>
+    </li>
+  </ul>
+  </li>
+
+<li>
+  <a href="#">
+    <div class="item-content">
+      <div class="item-media">
+        <i class="ion ion-ios-person"></i>
+      </div>
+      <div class="item-inner">
+        <span class="title"> Others </span>
+      </div>
+    </div>
+  </a>
+</li>
+
+<li>
+  <a href="#">
+    <div class="item-content">
+      <div class="item-media">
+        <i class="ion ion-ios-person"></i>
+      </div>
+      <div class="item-inner">
+        <span class="title"> Our projects </span>
+      </div>
+    </div>
+  </a>
+</li>
+
+<li>
+  <a href="#">
+    <div class="item-content">
+      <div class="item-media">
+        <i class="ion ion-ios-person"></i>
+      </div>
+      <div class="item-inner">
+        <span class="title"> Our projects </span>
+      </div>
+    </div>
+  </a>
+</li>
+
+<li>
+  <a href="#">
+    <div class="item-content">
+      <div class="item-media">
+        <i class="ion ion-ios-person"></i>
+      </div>
+      <div class="item-inner">
+        <span class="title"> Our projects </span>
+      </div>
+    </div>
+  </a>
+</li>
+
+<li>
+  <a href="#">
+    <div class="item-content">
+      <div class="item-media">
+        <i class="ion ion-ios-person"></i>
+      </div>
+      <div class="item-inner">
+        <span class="title"> Our projects </span>
+      </div>
+    </div>
+  </a>
+</li>
+
+<li>
+  <a href="#">
+    <div class="item-content">
+      <div class="item-media">
+        <i class="ion ion-ios-person"></i>
+      </div>
+      <div class="item-inner">
+        <span class="title"> Our projects </span>
+      </div>
+    </div>
+  </a>
+</li>
+
+<li>
+  <a href="#">
+    <div class="item-content">
+      <div class="item-media">
+        <i class="ion ion-ios-person"></i>
+      </div>
+      <div class="item-inner">
+        <span class="title"> Our projects </span>
+      </div>
+    </div>
+  </a>
+</li>
+
+<li>
+  <a href="#">
+    <div class="item-content">
+      <div class="item-media">
+        <i class="ion ion-ios-person"></i>
+      </div>
+      <div class="item-inner">
+        <span class="title"> Our projects </span>
+      </div>
+    </div>
+  </a>
+</li>
 
 
 
@@ -532,7 +787,7 @@ footer{
 <div class="navigation" id="navigation">
   <div class="nav-brand-left flex">
     <div class="brand-text-left">Admin</div>
-  <div class="nav-brand-left-icon">&#9776;</div>
+  <div class="nav-brand-left-icon" id="nav-brand-left-icon">&#9776;</div>
 </div>
    <ul class="menu-nav flex justify-right" id="menu-nav">
 <li id="float-right-on-navbar">
@@ -554,8 +809,8 @@ footer{
   <li id="float-right-on-navbar"><a href="#"><i  id="message-icon" class="ion ion-android-textsms"></i> Message  <span id="badge_count" class="badge_count">1</span>
 </a></li>
 
-</ul>
-  <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
+</ul>                                                               <!--9783-->
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9946;</a>
 </div>
 
 
@@ -563,10 +818,12 @@ footer{
 
 
 
-<div style="padding-left:16px">
+<div class="main-content">
+  <div class="wrap-content">
   <h2>Responsive Topnav with Dropdown</h2>
   <p>Resize the browser window to see how it works.</p>
   <p>Hover over the dropdown button to open the dropdown menu.</p>
+</div>
 </div>
 
 
@@ -599,18 +856,106 @@ function myFunction() {
 );
 
 function Configer_dropdown() {
-  const customdropdown = document.querySelectorAll('.custom-dropdown');
+  const customdropdown = document.querySelectorAll('.custom-dropdown-menu');
+  const openDropdown = document.querySelectorAll('.sub-menu');
   for (var i = 0; i < customdropdown.length; i++) {
       customdropdown[i].onclick = function () {
-          console.log(this);//this gets the parent element which event is appended
+        //  console.log(this);//this gets the parent element which event is appended
     // var dropdowns = this.getElementsByClassName("sub-menu")[0].className = 'sub-menu ';
+
+   
+            //open or close currently clicked
     let c = this.getElementsByClassName("sub-menu")[0].className;
-    if(c.match(/open/gi)) c = "sub-menu";
-    else c = "sub-menu open";
+   // console.log(c);
+    let cd = this.className;//parent class with event//custom-dropdown-menu
+    //console.log(cd);
+    if(c.match(/open/gi)){
+     c = "sub-menu";//remove open class and add sub-menu class
+     cd="custom-dropdown-menu";
+    }else{
+         //let's first close all the open dropdowns
+         for (var j = 0; j < openDropdown.length; j++) {
+          //remove open class and add sub-menu class
+         document.getElementsByClassName("sub-menu")[j].className="sub-menu";
+         document.getElementsByClassName("custom-dropdown-menu")[j].className="custom-dropdown-menu";
+         }
+         c = "sub-menu open"; //add open class and add sub-menu class
+          cd="custom-dropdown-menu open";
+         }
     this.getElementsByClassName("sub-menu")[0].className = c;
+     this.className = cd; //set class with event//custom-dropdown-menu
     }
   }
   }
+
+
+document.getElementById('nav-brand-left-icon').onclick = function () {
+let element = this;
+ let Sidebar = document.getElementById('sidebar');
+//console.log(Sidebar.className);
+let Sidebar_C = Sidebar.className;
+
+if(Sidebar_C.match(/sidebar-closed/gi)){
+//remove the classes
+Sidebar.classList.remove("sidebar-closed");//remove aclass
+//Sidebar.className = "sidebar"; // same as first one
+document.getElementsByClassName('main-content')[0].classList.remove("main-content-sidebar-closed");
+document.getElementsByClassName('brand-text-left')[0].classList.remove("sidebar-closed-brandtext-left");
+document.getElementsByClassName('nav-brand-left-icon')[0].classList.remove("sidebar-closed-brand-left-icon");
+document.getElementsByClassName('nav-brand-left')[0].classList.remove("nav-brand-left-sidebar-closed");
+document.getElementsByClassName('navbar-title')[0].classList.remove("navbar-title-sidebar-closed");
+
+var Iteminner = document.getElementsByClassName('item-inner');
+for (var i = 0; i < Iteminner.length; i++) {
+    Iteminner[i].classList.remove("item-inner-sidebar-closed");
+}
+var Itemcontent = document.getElementsByClassName('item-content');
+for (var i = 0; i < Itemcontent.length; i++) {
+    Itemcontent[i].classList.remove("item-content-sidebar-closed");
+}
+var Customdropdown = document.getElementsByClassName('custom-dropdown-menu');
+for (var i = 0; i < Customdropdown.length; i++) {
+    Customdropdown[i].classList.remove("custom-dropdown-sidebar-closed");
+}
+var Submenu = document.getElementsByClassName('sub-menu');
+for (var i = 0; i < Submenu.length; i++) {
+    Submenu[i].classList.remove("sub-menu-sidebar-closed");
+}
+
+
+}else{
+//for adding classes
+//Sidebar.classList.add("sidebar-closed"); //add a class
+Sidebar.className += " sidebar-closed"; // same as first one
+document.getElementsByClassName('main-content')[0].className += " main-content-sidebar-closed";
+document.getElementsByClassName('brand-text-left')[0].className += " sidebar-closed-brandtext-left";
+document.getElementsByClassName('nav-brand-left-icon')[0].className += " sidebar-closed-brand-left-icon";
+document.getElementsByClassName('nav-brand-left')[0].className += " nav-brand-left-sidebar-closed";
+document.getElementsByClassName('navbar-title')[0].className += " navbar-title-sidebar-closed";
+
+var Iteminner = document.getElementsByClassName('item-inner');
+for (var i = 0; i < Iteminner.length; i++) {
+    Iteminner[i].className += " item-inner-sidebar-closed";
+}
+var Itemcontent = document.getElementsByClassName('item-content');
+for (var i = 0; i < Itemcontent.length; i++) {
+    Itemcontent[i].className += " item-content-sidebar-closed";
+}
+
+var Customdropdown = document.getElementsByClassName('custom-dropdown-menu');
+for (var i = 0; i < Customdropdown.length; i++) {
+    Customdropdown[i].className += " custom-dropdown-sidebar-closed";
+}
+var Submenu = document.getElementsByClassName('sub-menu');
+for (var i = 0; i < Submenu.length; i++) {
+    Submenu[i].className += " sub-menu-sidebar-closed";
+}
+
+}
+
+
+
+}
 
 
 </script>
