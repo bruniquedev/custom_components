5fcc6c595398142e5119bd37bba85d83fdad86e6
Geofrey Bruno <bruniquedevelopers@gmail.com>
2023-03-25 16:33:13 +0300

    added a third pro interface

--- homepages/index_pro3.php
+++ homepages/index_pro3.php
@@ -78,8 +78,136 @@ body{
 .justify-center {
     justify-content: center;
 }
+.center{
+    text-align: center;
+}
 
+.section-pro-3{
+    min-height: 291px;
+    margin-top: 51px;
+}
 
+.section-pro-items-3{
+
+}
+.pro-item-3{
+width: 50%;
+min-width: 50%;
+}
+.pro-u-image{
+background-image: url(images/c-min.jpg);
+    background-position: 50% 50%;
+    min-height: 590px;
+    object-fit: cover;
+    display: block;
+    vertical-align: middle;
+    background-size: cover;
+    background-position: 50% 50%;
+    background-repeat: no-repeat;
+}
+
+.text-white{
+ color: #fff;   
+}
+.bg-paleblue{
+background-color: #68b1c0;
+}
+
+.items-content{
+padding-left: 30px;
+    padding-right: 30px;
+        padding: 30px 20px;
+}
+
+.items-repeaters{
+padding: 0px;
+}
+
+.u-repeater-item{
+margin-top: 35px;
+}
+.pro-icon-circle{
+    height: 40px;
+    width: 40px;
+    background-image: none;
+padding: 8px;
+color: #329dbb !important;
+background-color: #ffffff;
+border-radius: 50%;
+    display: block;
+    line-height: 0;
+    border-width: 0px;
+}
+.pro-icon-circle i{
+ text-align: center;
+    font-size: 50px;
+    margin-top: -7px;
+}
+.repeater-item-content{
+width: 365px;
+margin-left: 20px;
+}
+.pro-heading{
+font-size: 1.25rem;
+line-height: 1.2;
+margin-top: 0;
+font-weight: 700;
+margin-bottom: 0;
+word-wrap: break-word;
+color: #ffffff !important;
+}
+.pro-text{
+margin-left: 0;
+    margin-bottom: 0;
+    margin-top: 20px;
+ font-size: 1.125rem;
+    line-height: 1.6;
+    word-wrap: break-word; 
+    color: #ffffff !important;  
+}
+
+.margin_top_50{
+    margin-top: 50px;
+}
+
+.text-link{
+    margin-left: 72px;
+}
+
+.pro-text-link-3{
+color: #ffffff;
+display: inline;
+border-width: 1px;
+border-style: none none solid;
+    padding: 0;
+    border-color: #ffffff;
+    background-color: transparent;
+    text-decoration: none;
+}
+.pro-text-link-3:hover{
+  border: none !important;  
+}
+
+@media screen and (max-width: 600px){
+.pro-item-3 {
+    width: 100%;
+    min-width: 100%;
+}
+}
+
+@media screen and (max-width: 412px){
+.repeater-item-content {
+    width: 218px;
+    margin-left: 5px;
+}
+}
+
+ @media only screen and (min-width: 767px) and (max-width: 1000px)  {
+.repeater-item-content {
+    width: 280px;
+    margin-left: 8px;
+}
+}
 
 </style>
 </head>
@@ -89,7 +217,70 @@ body{
  <div class="top-nav-dashb"></div>
 
 
-    
+
+    <section class="section-pro-3 bg-paleblue">
+
+   
+          <div class="section-pro-items-3 flex flex-grow flex-wrap">
+
+            <div class="pro-item-3">
+              <div class="pro-u-image"></div>
+            </div>
+
+            <div class="pro-item-3 bg-paleblue text-white">
+
+              <div class="items-content">
+
+                <div class="items-repeaters">
+
+                  <div class="u-repeater-item flex flex-grow flex-wrap">
+                    <span class="pro-icon-circle"><i class="ion ion-android-star"></i></span>
+                        <div class="repeater-item-content">
+                          <h5 class="pro-heading">Sample Headline</h5>
+                          <p class="pro-text">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+                      </div>
+                  </div>
+
+                   <div class="u-repeater-item flex flex-grow flex-wrap">
+                    <span class="pro-icon-circle"><i class="ion ion-android-star"></i></span>
+                        <div class="repeater-item-content">
+                          <h5 class="pro-heading">Sample Headline</h5>
+                          <p class="pro-text">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+                      </div>
+                  </div>
+
+                   <div class="u-repeater-item flex flex-grow flex-wrap">
+                    <span class="pro-icon-circle"><i class="ion ion-android-star"></i></span>
+                        <div class="repeater-item-content">
+                          <h5 class="pro-heading">Sample Headline</h5>
+                          <p class="pro-text">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+                      </div>
+                  </div>
+                    
+                     <div class="u-repeater-item flex flex-grow flex-wrap">
+                    <span class="pro-icon-circle"><i class="ion ion-android-star"></i></span>
+                        <div class="repeater-item-content">
+                          <h5 class="pro-heading">Sample Headline</h5>
+                          <p class="pro-text">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+                      </div>
+                  </div> 
+
+
+                    <p class="pro-heading margin_top_50 text-link">Image by <a href="https://www.freepik.com/psd/mockup" class="pro-text-link-3">Freepik</a>
+                </p>
+
+                </div>
+
+              
+              </div>
+
+
+            </div>
+
+
+          </div>
+
+    </section>
 
 
 
